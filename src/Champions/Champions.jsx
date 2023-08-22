9767aaf7448c9fa79162e64d15801c103baf3654
ahouston <alexehouston@gmail.com>
2023-08-22 12:09:34 -0500

    update champions

--- src/Champions/Champions.jsx
+++ src/Champions/Champions.jsx
@@ -56,6 +56,148 @@ export default function Champions() {
     return name.replace(/[^\w]/g, "");
   };
 
+  const championsWithExtraAlignment = [
+    "Annie",
+    "Azir",
+    "Brand",
+    "Braum",
+    "Caitlyn",
+    "Camille",
+    "Corki",
+    "Draven",
+    "Ekko",
+    "Elise",
+    "Fizz",
+    "Gangplank",
+    "Garen",
+    "Gnar",
+    "Graves",
+    "Heimerdinger",
+    "Janna",
+    "Jarvan IV",
+    "Jax",
+    "Jayce",
+    "Jinx",
+    "Karma",
+    "Kayle",
+    "Kennen",
+    "Kha'Zix",
+    "Kindred",
+    "Kled",
+    "LeBlanc",
+    "Leona",
+    "Lissandra",
+    "Lulu",
+    "Master Yi",
+    "Miss Fortune",
+    "Morgana",
+    "Nami",
+    "Nautilus",
+    "Nidalee",
+    "Orianna",
+    "Riven",
+    "Ryze",
+    "Shaco",
+    "Shen",
+    "Singed",
+    "Sion",
+    "Skarner",
+    "Syndra",
+    "Taliyah",
+    "Taric",
+    "Tryndamere",
+    "Twisted Fate",
+    "Vayne",
+    "Vi",
+    "Viego",
+    "Vladimir",
+    "Yasuo",
+    "Ziggs",
+    "Zyra",
+  ];
+  
+  const championsWithAdjustedAlignment = [
+    "Aatrox",
+    "Ahri",
+    "Akshan",
+    "Alistar",
+    "Amumu",
+    "Ashe",
+    "Bard",
+    "Blitzcrank",
+    "Cassiopeia",
+    "Diana",
+    "Ezreal",
+    "Fiora",
+    "Gragas",
+    "Hecarim",
+    "Heimerdinger",
+    "Illaoi",
+    "Jhin",
+    "Kalista",
+    "Karma",
+    "Karthus",
+    "Kassadin",
+    "Katarina",
+    "Kindred",
+    "Kog'Maw",
+    "LeBlanc",
+    "Lee Sin",
+    "Lissandra",
+    "Lucian",
+    "Lux",
+    "Malphite",
+    "Malzahar",
+    "Maokai",
+    "Wukong",
+    "Nasus",
+    "Nilah",
+    "Nocturne",
+    "Nunu & Willump",
+    "Olaf",
+    "Poppy",
+    "Quinn",
+    "Rammus",
+    "Renata Glasc",
+    "Renekton",
+    "Rengar",
+    "Rumble",
+    "Samira",
+    "Sejuani",
+    "Shyvana",
+    "Sona",
+    "Swain",
+    "Tahm Kench",
+    "Talon",
+    "Teemo",
+    "Thresh",
+    "Tristana",
+    "Trundle",
+    "Urgot",
+    "Veigar",
+    "Vel'Koz",
+    "Vex",
+    "Viktor",
+    "Volibear",
+    "Warwick",
+    "Xayah",
+    "Xerath",
+    "Yone",
+    "Yorick",
+    "Zac",
+    "Zed",
+  ];
+
+  const getAlignmentClass = (name) => {
+    if (championsWithExtraAlignment.includes(name)) {
+      return "extra-alignment";
+    } else if (championsWithAdjustedAlignment.includes(name)) {
+      return "adjust-alignment";
+    } else {
+      return "";
+    }
+  };
+
   return (
     <div className="Champions d-flex flex-column align-items-center justify-content-center w-100 animate__animated animate__fadeIn">
       <div className="champion-header d-flex flex-column justify-content-center align-items-center">
@@ -65,7 +207,11 @@ export default function Champions() {
           alt="Chamions"
         />
         <div className="champion-header-inner d-flex align-items-center mt-3 mb-5">
-          <img className="line-reflected" src="/assets/misc/line.png" alt="Chamions" />
+          <img
+            className="line-reflected"
+            src="/assets/misc/line.png"
+            alt="Chamions"
+          />
           <h1 className="d-inline text-uppercase mx-4">Champions</h1>
           <img src="/assets/misc/line.png" alt="Chamions" />
         </div>
@@ -73,7 +219,9 @@ export default function Champions() {
       <div className="d-flex flex-wrap justify-content-center w-75">
         {Object.values(champions).map((champion) => (
           <div
-            className="champion overflow-hidden d-flex justify-content-center mx-2 my-4 position-relative"
+            className={`champion overflow-hidden d-flex justify-content-center mx-2 my-4 position-relative ${getAlignmentClass(
+              champion.name
+            )}`}
             key={champion.id}
           >
             <img
